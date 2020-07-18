83b124aa5fda3dc4f2a827f322cb7b13b66b1a1e
sumibi-yakitori <nekosoft360@gmail.com>
2020-07-18 11:23:30 +0900

    First commit.

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate lazy_static;
+
+use reqwest::blocking::Client as BlockingClient;
+use reqwest::Client;
+
+lazy_static! {
+  static ref CLIENT: Client = Client::new();
+  static ref BLOCKING_CLIENT: BlockingClient = BlockingClient::new();
+}
+
+pub fn client() -> &'static Client {
+  &CLIENT
+}
+
+pub fn blocking_client() -> &'static BlockingClient {
+  &BLOCKING_CLIENT
+}
